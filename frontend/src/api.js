bd60f50184c85171c61511f9343550e3cc234263
SuyashPant04 <126603072+SuyashPant04@users.noreply.github.com>
2025-03-23 22:48:33 +0530

    added .env for frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: import.meta.env.VITE_BASE_URL, // Accessing the environment variable
     withCredentials: true,
 });
 
 export const shortenURL = (formData) => API.post('/shorten', formData);
 export const redirectURL = (shortCode) => API.get(`/${shortCode}`);
+
+// import axios from 'axios';
+
+// const API = axios.create({
+//     baseURL: "http://localhost:8000",
+//     withCredentials: true,
+// });
+
+// export const shortenURL = (formData) => API.post('/shorten', formData);
+// export const redirectURL = (shortCode) => API.get(`/${shortCode}`);
